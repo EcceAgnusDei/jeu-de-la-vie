d4011277fc630bda183f6ba0091e4d4585ead33f
EcceAgnusDei <mondo.antoine@yahoo.fr>
2019-09-17 12:35:00 +0200

    Ajout d'un lieu vers les api relatif

--- src/apiPath.js
+++ src/apiPath.js
@@ -1,4 +1,5 @@
 const onlineApiPath = 'https://jeu-de-la-vie.mondoloni-dev.fr/golApi/';
 const localApiPath = 'http://localhost/jeu-de-la-vie/golApi/';
+const relativePath = '/golApi/';
 
-export default onlineApiPath;
+export default relativePath;
